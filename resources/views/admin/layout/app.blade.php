c93e76fdc3ae3aa625c9b3d1fa5a2c4c02f4b9d4
tutul <tutul5162@gmail.com>
2022-07-18 19:02:17 +0600

    sodk sidj

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -8,21 +8,21 @@
         <title>One-Health - Dashboard</title>
 		
 		<!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="{{url('admin/assets/img/favicon.png')}}">
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('admin/assets/img/favicon.png')}}">
 		
 		<!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{url('admin/assets/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('admin/assets/css/bootstrap.min.css')}}">
 		
 		<!-- Fontawesome CSS -->
-        <link rel="stylesheet" href="{{url('admin/assets/css/font-awesome.min.css')}}">
+        <link rel="stylesheet" href="{{asset('admin/assets/css/font-awesome.min.css')}}">
 		
 		<!-- Feathericon CSS -->
-        <link rel="stylesheet" href="{{url('admin/assets/css/feathericon.min.css')}}">
+        <link rel="stylesheet" href="{{asset('admin/assets/css/feathericon.min.css')}}">
 		
-		<link rel="stylesheet" href="{{url('admin/assets/plugins/morris/morris.css')}}">
+		<link rel="stylesheet" href="{{asset('admin/assets/plugins/morris/morris.css')}}">
 		
 		<!-- Main CSS -->
-        <link rel="stylesheet" href="{{url('admin/assets/css/style.css')}}">
+        <link rel="stylesheet" href="{{asset('admin/assets/css/style.css')}}">
 		
 		<!--[if lt IE 9]>
 			<script src="admin/assets/js/html5shiv.min.js"></script>
@@ -53,21 +53,21 @@
 		<!-- /Main Wrapper -->
 		
 		<!-- jQuery -->
-        <script src="{{url('admin/assets/js/jquery-3.2.1.min.js')}}"></script>
+        <script src="{{asset('admin/assets/js/jquery-3.2.1.min.js')}}"></script>
 		
 		<!-- Bootstrap Core JS -->
-        <script src="{{url('admin/assets/js/popper.min.js')}}"></script>
-        <script src="{{url('admin/assets/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('admin/assets/js/popper.min.js')}}"></script>
+        <script src="{{asset('admin/assets/js/bootstrap.min.js')}}"></script>
 		
 		<!-- Slimscroll JS -->
-        <script src="{{url('admin/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+        <script src="{{asset('admin/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
 		
-		<script src="{{url('admin/assets/plugins/raphael/raphael.min.js')}}"></script>    
-		<script src="{{url('admin/assets/plugins/morris/morris.min.js')}}"></script>  
-		<script src="{{url('admin/assets/js/chart.morris.js')}}"></script>
+		<script src="{{asset('admin/assets/plugins/raphael/raphael.min.js')}}"></script>    
+		<script src="{{asset('admin/assets/plugins/morris/morris.min.js')}}"></script>  
+		<script src="{{asset('admin/assets/js/chart.morris.js')}}"></script>
 		
 		<!-- Custom JS -->
-		<script  src="{{url('admin/assets/js/script.js')}}"></script>
+		<script  src="{{asset('admin/assets/js/script.js')}}"></script>
 		
     </body>
 
